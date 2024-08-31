1d1836f8ee00052a3e19cc84b853deb82ea151ff
Vicente Mazzaro <vicentemazzaro@hotmail.com>
2024-08-31 17:52:51 -0300

    chore(app): Update API URL configuration

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
+  apiUrl: process.env.VUE_APP_API_URL || 'http://localhost:3000/api',
   infuraUrl: process.env.VUE_APP_INFURA_URL || 'https://sepolia.infura.io/v3',
   infuraProjectId: process.env.VUE_APP_INFURA_PROJECT_ID || 'YOUR_INFURA_PROJECT_ID',
   contractAddress: process.env.VUE_APP_CONTRACT_ADDRESS || 'YOUR_CONTRACT_ADDRESS',
--- app/src/services/apiService.ts
+++ app/src/services/apiService.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosResponse } from 'axios';
+import { config } from '../config';
 
-const API_URL = 'http://localhost:3000/api';
+const { apiUrl } = config;
 
 type NFT = {
   tokenId: number
@@ -32,21 +33,21 @@ interface GetLatestNFTResponse extends BaseResponse {
 }
 
 export const mintNFT = async (to: string, tokenURI: string): Promise<AxiosResponse<MintNFTResponse>> => {
-  return axios.post<MintNFTResponse>(`${API_URL}/mint`, { to, tokenURI });
+  return axios.post<MintNFTResponse>(`${apiUrl}/mint`, { to, tokenURI });
 };
 
 export const transferNFT = async (from: string, to: string, tokenId: number): Promise<AxiosResponse<TransferNFTResponse>> => {
-  return axios.post<TransferNFTResponse>(`${API_URL}/transfer`, { from, to, tokenId });
+  return axios.post<TransferNFTResponse>(`${apiUrl}/transfer`, { from, to, tokenId });
 };
 
 export const uploadMetadata = async (metadata: object): Promise<AxiosResponse<UploadMetadataResponse>> => {
-  return axios.post<UploadMetadataResponse>(`${API_URL}/uploadMetadata`, metadata);
+  return axios.post<UploadMetadataResponse>(`${apiUrl}/uploadMetadata`, metadata);
 };
 
 export const getNFTs = async (address: string): Promise<AxiosResponse<GetNFTsResponse>> => {
-  return axios.get<GetNFTsResponse>(`${API_URL}/nfts/${address}`);
+  return axios.get<GetNFTsResponse>(`${apiUrl}/nfts/${address}`);
 };
 
 export const getLatestNFT = async (address: string): Promise<AxiosResponse<GetLatestNFTResponse>> => {
-  return axios.get<GetLatestNFTResponse>(`${API_URL}/nfts/${address}/latest`);
+  return axios.get<GetLatestNFTResponse>(`${apiUrl}/nfts/${address}/latest`);
 };
